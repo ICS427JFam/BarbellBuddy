7f1a4536322e278548309e28f97ba871880ee41a
Tysen Imai-Toyama <42785023+TysenImaiToyama@users.noreply.github.com>
2020-03-08 01:59:39 -1000

    Register Page and Express server
    Created register page and express server backend. Routed backend server to front end

---
+++ server/user.js
@@ -0,0 +1,18 @@
+// /backend/data.js
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// this will be our data base's data structure
+const DataSchema = new Schema(
+  {
+    id: Number,
+    firstName: String,
+    lastName: String,
+    email: String,
+    password: String,
+  },
+  { timestamps: true }
+);
+
+// export the new Schema so we could modify it using Node.js
+module.exports = mongoose.model("User", DataSchema);
