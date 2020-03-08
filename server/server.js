44830c782883f91032c0f9238f23f98abc1212b9
Tysen Imai-Toyama <42785023+TysenImaiToyama@users.noreply.github.com>
2020-03-08 02:08:19 -1000

    Clean functions
    Deleted old functions for new functions for Barbell Buddy REST API

--- server/server.js
+++ server/server.js
@@ -30,54 +30,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(logger('dev'));
 
-// this is our get method
-// this method fetches all available data in our database
-router.get('/getUser', (req, res) => {
-  User.find((err, data) => {
-    if (err) return res.json({ success: false, error: err });
-    return res.json({ success: true, data: data });
-  });
-});
-
-// this is our update method
-// this method overwrites existing data in our database
-router.post('/updateUser', (req, res) => {
-  const { id, update } = req.body;
-  User.findByIdAndUpdate(id, update, (err) => {
-    if (err) return res.json({ success: false, error: err });
-    return res.json({ success: true });
-  });
-});
-
-// this is our delete method
-// this method removes existing data in our database
-router.delete('/deleteUser', (req, res) => {
-  const { id } = req.body;
-  User.findByIdAndRemove(id, (err) => {
-    if (err) return res.send(err);
-    return res.json({ success: true });
-  });
-});
-
-// this is our create methid
-// this method adds new data in our database
-router.post('/User', (req, res) => {
-  let data = new User();
-
-  const { id, message } = req.body;
-
-  if ((!id && id !== 0) || !message) {
-    return res.json({
-      success: false,
-      error: 'INVALID INPUTS',
-    });
-  }
-  data.message = message;
-  data.id = id;
-  data.save((err) => {
-    if (err) return res.json({ success: false, error: err });
-    return res.json({ success: true });
-  });
+app.get('/', (req, res) => {
+  res.send("Barbell Buddy Express Server");
 });
 
 // append /api for our http requests
